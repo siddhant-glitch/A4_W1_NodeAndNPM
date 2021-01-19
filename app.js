6d79ad6917cd84d33e378da31d22f05d8a1ac77d
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2021-01-19 12:18:33 -0500

    fixing for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http'); //like a php require
 // require is more orless the same as js import
 
 const hostname = '127.0.0.1'; //this is localhost
-const port = 3000;  //localhost:3000
+const port = process.env.PORT || 3000;  //localhost:3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
